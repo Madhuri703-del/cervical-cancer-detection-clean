b80545a513179d45c4a051933c807a255ed812b8
Challa Madhuri <madhurichalla703@gmail.com@gmail.com>
2026-04-27 13:01:09 +0530

    updated for deployment

--- app.py
+++ app.py
@@ -78,6 +78,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    print("🚀 Starting Flask App...")
-    model = load_model("model.keras")
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
